cf3924aa1ee735645c6ca0647f13a6b6365998e1
Brian Barbe <brian@stack.care>
2022-04-08 22:39:42 -0700

    Add file_status * Create file_status.py to update file state

---
+++ vm/file_status.py
@@ -0,0 +1,36 @@
+import time
+from function_apis import get_files, update_file
+from torrent_apis import get_torrents, get_torrent_by_id
+
+
+def main():
+    while True:
+        active = get_files('in_progress')
+        torrents = get_torrents()
+
+        if not active or not torrents:
+            return
+
+        for file in active:
+            tor = get_torrent_by_id(torrents, file['tor_hash'])
+            if not tor:
+                print(f'No torrent found for {file}')
+                continue
+
+            status = 'in_progress'
+            if tor.date_completed:
+                status = 'completed'
+
+            update_file(file, {
+                'status': status,
+                'tor_name': tor.name,
+                'tor_size': tor.size,
+                'tor_status_message': tor.status_message,
+                'tor_progress': tor.percent_progress,
+                'tor_date_completed': tor.date_completed
+            })
+        time.sleep(15)
+
+
+if __name__ == '__main__':
+    main()
